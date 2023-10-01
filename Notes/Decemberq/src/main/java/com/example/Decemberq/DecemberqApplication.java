14725f09579400c4742161a858ffcbdf9415e695
Umaam2000 <93532064+Umaam2000@users.noreply.github.com>
2023-10-01 22:33:56 +0530

    Add files via upload

---
+++ Notes/Decemberq/src/main/java/com/example/Decemberq/DecemberqApplication.java
@@ -0,0 +1,13 @@
+package com.example.Decemberq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecemberqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecemberqApplication.class, args);
+	}
+
+}
---
+++ Notes/Decemberq/src/test/java/com/example/Decemberq/DecemberqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Decemberq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecemberqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
